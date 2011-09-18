2703c5eba296d210ff55ec90486f17123d1dc4d8
Robert Diamond <rmd6502@gmail.com>
2011-09-17 20:29:49 -0400

    matrix

--- gen/com/diamond/matrix/R.java
+++ gen/com/diamond/matrix/R.java
@@ -14,28 +14,31 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int addColumn=0x7f060009;
-        public static final int addColumnButton=0x7f060002;
-        public static final int addRow=0x7f06000a;
-        public static final int addRowButton=0x7f060003;
-        public static final int cancel=0x7f06000b;
-        public static final int delColumn=0x7f060007;
-        public static final int delRow=0x7f060008;
-        public static final int gridView1=0x7f060001;
-        public static final int linearLayout1=0x7f060000;
-        public static final int linearLayout2=0x7f060004;
-        public static final int solveButton=0x7f060005;
-        public static final int transposeButton=0x7f060006;
+        public static final int addColumn=0x7f070009;
+        public static final int addColumnButton=0x7f070002;
+        public static final int addRow=0x7f07000a;
+        public static final int addRowButton=0x7f070003;
+        public static final int cancel=0x7f07000b;
+        public static final int delColumn=0x7f070007;
+        public static final int delRow=0x7f070008;
+        public static final int gridView1=0x7f070001;
+        public static final int linearLayout1=0x7f070000;
+        public static final int linearLayout2=0x7f070004;
+        public static final int solveButton=0x7f070005;
+        public static final int transposeButton=0x7f070006;
     }
     public static final class layout {
         public static final int grid_text_view=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int popup=0x7f050000;
+        public static final int popup=0x7f060000;
+    }
+    public static final class raw {
+        public static final int matrix=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
